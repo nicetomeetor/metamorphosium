c46ac3fcb2470b86f1e8dfb9ff4d415315b4efcd
Michael Lutsiv <nicetomeetor@gmail.com>
2023-05-01 14:54:33 +0300

    improve selection filtering

--- lib/Filter.ts
+++ lib/Filter.ts
@@ -1,22 +1,70 @@
 import { quantile } from 'simple-statistics';
 
+import { CollectorResult, Selection } from './types';
+
 export default class Filter {
-  public static process(array: number[]): number[] {
+  public static process(
+    firstResult: CollectorResult,
+    secondResult: CollectorResult
+  ): [CollectorResult, CollectorResult] {
+    for (const task of Object.keys(firstResult)) {
+      const firstFilteredSelection = Filter.execute(firstResult[task]);
+      const secondFilteredSelection = Filter.execute(secondResult[task]);
+
+      const [first, second] = Filter.match(
+        firstFilteredSelection,
+        secondFilteredSelection
+      );
+
+      firstResult[task] = first;
+      secondResult[task] = second;
+    }
+
+    return [firstResult, secondResult];
+  }
+
+  public static match(
+    firstSelection: Selection,
+    secondSelection: Selection
+  ): [Selection, Selection] {
+    const first = [];
+    const second = [];
+
+    const minLength = Math.min(firstSelection.length, secondSelection.length);
+
+    let j = 0;
+
+    for (let i = 0; i < minLength; i++) {
+      if (isNaN(firstSelection[i]) || isNaN(secondSelection[i])) {
+        j++;
+        continue;
+      }
+
+      first[i - j] = firstSelection[i];
+      second[i - j] = secondSelection[i];
+    }
+
+    return [first, second];
+  }
+
+  private static execute(array: Selection): Selection {
     const q1 = quantile(array, 0.25);
     const q3 = quantile(array, 0.75);
+
     const iqr = q3 - q1;
+
     const coef = 1.5;
+
     const maxValue = q3 + iqr * coef;
     const minValue = q1 - iqr * coef;
 
     const result = [];
-    let diff = 0;
 
     for (let i = 0; i < array.length; i++) {
       if (array[i] <= maxValue && array[i] >= minValue) {
-        result[i - diff] = array[i];
+        result[i] = array[i];
       } else {
-        diff += 1;
+        result[i] = NaN;
       }
     }
 
--- test/filter.test.ts
+++ test/filter.test.ts
@@ -1,13 +1,63 @@
 import Filter from '../lib/Filter';
 
+import { CollectorResult } from '../lib/types';
+
+const createCollectorResult = (arr: number[]): CollectorResult => {
+  return {
+    parseHTML: arr,
+  };
+};
+
 test('', () => {
-  const arrayToFilter = [1, 2, 3, 4, 5, 6, 8, 9, 100];
-  const res = Filter.process(arrayToFilter);
-  expect(res.toString()).toBe([1, 2, 3, 4, 5, 6, 8, 9].toString());
+  const firstCollectorResult = createCollectorResult([1, 2, 3, 4, 5, 100]);
+
+  const secondCollectorResult = createCollectorResult([1, 2, 3, 4, 5, 100]);
+
+  const [firstFilteredSelection, secondFilteredSelection] = Filter.process(
+    firstCollectorResult,
+    secondCollectorResult
+  );
+
+  const arr = [1, 2, 3, 4, 5];
+
+  for (const value of Object.values(firstFilteredSelection)) {
+    expect(value.toString()).toBe(arr.toString());
+  }
+
+  for (const value of Object.values(secondFilteredSelection)) {
+    expect(value.toString()).toBe(arr.toString());
+  }
 });
 
 test('', () => {
-  const arrayToFilter = [1, 2, 3, 4, 5, 6, 8, 9];
-  const res = Filter.process(arrayToFilter);
-  expect(res.toString()).toBe([1, 2, 3, 4, 5, 6, 8, 9].toString());
+  const firstCollectorResult = createCollectorResult([1, 2, 100, 4, 5]);
+
+  const secondCollectorResult = createCollectorResult([1, 2, 3, 100, 5]);
+
+  const [firstFilteredSelection] = Filter.process(
+    firstCollectorResult,
+    secondCollectorResult
+  );
+
+  const arr = [1, 2, 5];
+
+  for (const value of Object.values(firstFilteredSelection)) {
+    expect(value.toString()).toBe(arr.toString());
+  }
+});
+
+test('', () => {
+  const arr = [0, 0, 0, 0, 0, 0];
+
+  const firstCollectorResult = createCollectorResult(arr);
+  const secondCollectorResult = createCollectorResult(arr);
+
+  const [firstFilteredSelection] = Filter.process(
+    firstCollectorResult,
+    secondCollectorResult
+  );
+
+  for (const value of Object.values(firstFilteredSelection)) {
+    expect(value.toString()).toBe(arr.toString());
+  }
 });
